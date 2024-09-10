2b6d1767c5ef3c7815462df64581201a549cec69
Mdmahadi71 <habibalmasud79@gmail.com>
2024-09-10 11:14:33 -0700

    demo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Container from './Container'
 import { Link } from 'react-router-dom'
+import ScrollToTop from "react-scroll-to-top";
 
 const Footer = () => {
   return (
@@ -11,6 +12,9 @@ const Footer = () => {
                 <div className=" py-[30px] text-center lg:text-start">
                     <h3 className=' text-[20px] text-white font-semibold font-bangla'>সরকারি শাহ্ আব্দুর রউফ কলেজ</h3>
                     <p className=' font-medium text-white text-[15px] font-bangla'>পীরগঞ্জ, রংপুর</p>
+                </div>
+                <div className=" w-full">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20514.279503420355!2d89.2898861493001!3d25.43002572100525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fcdbceb2c86da9%3A0xfe20f5dc599f7327!2sGovt.%20Shah%20Abdur%20Rouf%20College!5e0!3m2!1sen!2sbd!4v1725991595970!5m2!1sen!2sbd" width="250px"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
             <div className="lg:w-[18%]">
@@ -56,6 +60,7 @@ const Footer = () => {
             </div>
         </div>
       </Container>
+      <ScrollToTop color='#38bdf8' top='300' smooth />
     </div>
   )
 }
