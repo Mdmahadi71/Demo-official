de44631e9cf536765c4a20e723236e783ba2a425
Mdmahadi71 <habibalmasud79@gmail.com>
2024-09-11 06:41:38 -0700

    demo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,6 +10,7 @@ const Footer = () => {
         <div className=" lg:flex justify-between">
             <div className="lg:w-[18%]">
                 <div className=" py-[30px] text-center lg:text-start">
+                    
                     <h3 className=' text-[20px] text-white font-semibold font-bangla'>সরকারি শাহ্ আব্দুর রউফ কলেজ</h3>
                     <p className=' font-medium text-white text-[15px] font-bangla'>পীরগঞ্জ, রংপুর</p>
                 </div>
