a3b1894ef7e3ba62e3e7db5f3a0cf02c72200371
Mdmahadi71 <habibalmasud79@gmail.com>
2024-11-05 08:23:47 -0800

    demo

--- src/Pages/Principal.jsx
+++ src/Pages/Principal.jsx
@@ -9,6 +9,7 @@ const Principal = () => {
         <div className=" lg:flex justify-between mx-3 lg:mx-0">
           <div className="lg:w-[60%]">
             <h2 className=' font-semibold text-[25px] text-black font-bangla'>অধ্যক্ষের কথা</h2>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias commodi molestiae labore eius sed quidem placeat ducimus magni eum, sint sequi saepe porro nostrum ipsam neque modi, inventore possimus et.</h4>
           </div>
           <div className="lg:w-[38%]">
             <Sharee/>
